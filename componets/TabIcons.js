2337080dbbbab547ac463522bb5fe63146b77944
Kimyeongsan <apua34@gmail.com>
2022-01-17 15:28:30 +0900

    Feat: Trade Button Add

--- componets/TabIcons.js
+++ componets/TabIcons.js
@@ -5,16 +5,28 @@ import { FONTS, COLORS } from '../constants'
 
 const TabIcons = ({ focused, icon, iconStyle, label, isTrade }) => {
 
-    if(isTrade) {
-        return(
-            <View>
-                <Text style={{ color: COLORS.white, ...FONTS.h4}}>{label}</Text>
+    if (isTrade) {
+        return (
+            <View style={{ alignItems: 'center', justifyContent: 'center', width: 60, height: 60, borderRadius: 30, backgroundColor: COLORS.black }}>
+
+                <Image
+                    source={icon}
+                    resizeMode='contain'
+                    style={{
+                        width: 25,
+                        height: 25,
+                        tintColor: COLORS.white,
+                        ...iconStyle
+                    }}
+                />
+                <Text style={{ color: COLORS.white, ...FONTS.h4 }}>{label}</Text>
+
             </View>
         )
-        
+
     } else {
-        return(
-            <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+        return (
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
 
                 <Image
                     source={icon}
@@ -28,7 +40,7 @@ const TabIcons = ({ focused, icon, iconStyle, label, isTrade }) => {
                 >
 
                 </Image>
-                <Text style={{ color: focused ? COLORS.white : COLORS.secondary}}>{label}</Text>
+                <Text style={{ color: focused ? COLORS.white : COLORS.secondary }}>{label}</Text>
             </View>
         )
     }
